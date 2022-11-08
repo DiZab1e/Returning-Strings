ae8aeba9ad4cda0ef8b7359676c1957b056f1d9d
Григорий <fash1on.pro@yandex.ru>
2022-11-08 10:02:07 +0300

    mk upds

---
+++ returning strings.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static string Greet(string name)
+    {
+        return ($"Hello, {name} how are you doing today?");
+    }
+}
